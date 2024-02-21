fe9f53f3804f96dc57d6874348de8fb30bc38fc6
timelessnesses <timelessnesses@timelessnesses.me>
2024-02-21 21:31:08 +0700

    tooling

--- tools/change_password.py
+++ tools/change_password.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 import os
+import getpass
 
 import bcrypt
 
@@ -12,12 +13,18 @@ async def add():
     assert db != None, "Pool returns None?"
     print("Connected to database")
     user = input("Username: ")
+    password = getpass.getpass("Password: ")
+    salt = bcrypt.gensalt()
+    salted_password = bcrypt.hashpw(password.encode(), salt)
+    # d: asyncpg.Connection
     async with db.acquire() as d:
         await d.execute(
             """
-            DELETE FROM users WHERE username = $1
-            """,
+        UPDATE users SET password = $2, salt = $3 WHERE user = $1
+                        """,
             user,
+            salted_password.hex(),
+            salt.hex(),
         )
     print("Done!")
 
--- tools/remove_folder.py
+++ tools/remove_folder.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 import os
-import getpass
 
 import bcrypt
 
@@ -12,23 +11,13 @@ async def add():
     db = await asyncpg.create_pool(host=os.getenv("FOLDERISTIC_HOST"), user=os.getenv("FOLDERISTIC_USER"), password=os.getenv("FOLDERISTIC_PASS"), database=os.getenv("FOLDERISTIC_DB"))  # type: ignore
     assert db != None, "Pool returns None?"
     print("Connected to database")
-    user = input("Username: ")
-    password = getpass.getpass("Password: ")
-    roles = input("Roles: ")
-    if roles.lower() not in ["admin", "uploaders", "viewers"]:
-        assert False, "Role is not supported"
-    salt = bcrypt.gensalt()
-    salted_password = bcrypt.hashpw(password.encode(), salt)
-    # d: asyncpg.Connection
+    user = input("Folder ID: ")
     async with db.acquire() as d:
         await d.execute(
             """
-        INSERT INTO users(username, password, salt, roles) VALUES ($1, $2, $3, $4)
-                        """,
+            DELETE FROM folders WHERE id = $1
+            """,
             user,
-            salted_password.hex(),
-            salt.hex(),
-            roles,
         )
     print("Done!")
 
